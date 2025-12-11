1553af7059ba62492e11f7f96d386221c7908463
Rajesh Jondhale <jondhaleraj2151@gmail.com>
2025-12-11 13:36:44 +0530

    TMDB API data fetch && built mainContainer

---
+++ src/components/VideoBackground.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux'
+import useMovieTrailer from '../hooks/useMovieTrailer'
+
+
+const VideoBackground = ({ movieId }) => {
+
+    const trailerVideo = useSelector(store => store.movies?.trailerVideo)
+    useMovieTrailer(movieId);
+
+    return (
+        <div className='w-screen'>
+            <iframe
+                className='w-screen aspect-video'
+                src={'https://www.youtube.com/embed/' + trailerVideo?.key + '?&autoplay=1&mute=1&controls=0&rel=0&modestbranding=1&showinfo=0&disablekb=1&iv_load_policy=3&fs=0'}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+
+            >
+
+            </iframe></div>
+    )
+}
+
+export default VideoBackground
---
+++ src/hooks/useMovieTrailer.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+import { API_OPTIONS } from "../utils/constants.jsx"
+import { useEffect } from "react";
+
+const useMovieTrailer = (movieId) => {
+
+    const dispatch = useDispatch();
+
+    //fetching trailer video and upadating store with tailer data
+    const getMovieVideos = async () => {
+        const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`
+            , API_OPTIONS);
+        const json = await data.json();
+        // console.log(json);
+
+        const filterData = json.results.filter(video => video.type === "Trailer");
+        const trailer = filterData.length ? filterData[0] : json.results[0];
+        dispatch(addTrailerVideo(trailer))
+        // console.log(trailer);
+    }
+    useEffect(() => {
+        getMovieVideos();
+    }, [])
+}
+
+export default useMovieTrailer;
